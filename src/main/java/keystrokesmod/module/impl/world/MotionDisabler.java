83235e60c099cdda43cca7362bc6e3cb48c46933
Thj <103907969+fnfcreato@users.noreply.github.com>
2024-11-30 17:40:18 -0300

    Update MotionDisabler.java

--- src/main/java/keystrokesmod/module/impl/world/MotionDisabler.java
+++ src/main/java/keystrokesmod/module/impl/world/MotionDisabler.java
@@ -3,7 +3,6 @@ package keystrokesmod.module.impl.world;
 import keystrokesmod.event.PreMotionEvent;
 import keystrokesmod.event.ReceivePacketEvent;
 import keystrokesmod.module.Module;
-import keystrokesmod.module.ModuleManager;
 import keystrokesmod.utility.PacketUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
@@ -11,18 +10,12 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C03PacketPlayer;
 import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import keystrokesmod.module.setting.impl.ButtonSetting;
-import keystrokesmod.module.setting.impl.DescriptionSetting;
 import keystrokesmod.module.setting.impl.SliderSetting;
-import keystrokesmod.utility.Utils;
-
-
 
 public class MotionDisabler extends Module {
     private boolean isActive = false;
@@ -30,14 +23,13 @@ public class MotionDisabler extends Module {
     private int correctionCount = 0;
     private float progress = 0;
     private boolean motionRestored = false;
-    public SliderSetting a;
-    
-    public MotionDisabler() {
 
-        public MotionDisabler() {
-            super("MotionDisabler", category.world, 0);
-        }
-        
+    public SliderSetting a;
+
+    // did something wrong here i didnt saw it my bad.
+    public MotionDisabler() {
+        super("MotionDisabler", category.world, 0);
+    }
 
     @Override
     public void onEnable() {
@@ -63,6 +55,7 @@ public class MotionDisabler extends Module {
     @SubscribeEvent
     public void onPreMotion(PreMotionEvent event) {
         if (!isActive) return;
+
         if (!mc.thePlayer.onGround) {
             offGroundTicks++;
             if (offGroundTicks >= 10) {
@@ -101,16 +94,18 @@ public class MotionDisabler extends Module {
 
     private void drawBar(int x, int y, int width, int height, float percentage) {
         Tessellator tessellator = Tessellator.getInstance();
-        WorldRenderer buffer = tessellator.getWorldRenderer(); // Pre-1.13 Forge uses WorldRenderer
+        WorldRenderer buffer = tessellator.getWorldRenderer();
         GlStateManager.enableBlend();
         buffer.begin(7, DefaultVertexFormats.POSITION_COLOR);
+
+        // Background
         buffer.pos(x, y + height, 0).color(0, 0, 0, 150).endVertex();
         buffer.pos(x + width, y + height, 0).color(0, 0, 0, 150).endVertex();
         buffer.pos(x + width, y, 0).color(0, 0, 0, 150).endVertex();
         buffer.pos(x, y, 0).color(0, 0, 0, 150).endVertex();
         tessellator.draw();
 
-        // Draw the progress bar
+        // Progress
         buffer.begin(7, DefaultVertexFormats.POSITION_COLOR);
         buffer.pos(x, y + height, 0).color(0, 255, 0, 150).endVertex();
         buffer.pos(x + (width * percentage), y + height, 0).color(0, 255, 0, 150).endVertex();
