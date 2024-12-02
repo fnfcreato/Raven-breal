ed28727e1282f0362f5cd3890291f2c927689ca9
Thj <103907969+fnfcreato@users.noreply.github.com>
2024-12-02 12:32:18 -0300

    Update MixinEntityPlayerSP.java

--- src/main/java/keystrokesmod/mixins/impl/entity/MixinEntityPlayerSP.java
+++ src/main/java/keystrokesmod/mixins/impl/entity/MixinEntityPlayerSP.java
@@ -34,28 +34,57 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
     @Shadow
     public int sprintingTicksLeft;
 
-    public MixinEntityPlayerSP(World p_i45074_1_, GameProfile p_i45074_2_) {
-        super(p_i45074_1_, p_i45074_2_);
-    }
-
-    @Override
-    @Shadow
-    public abstract void setSprinting(boolean p_setSprinting_1_);
-
     @Shadow
     protected int sprintToggleTimer;
+
     @Shadow
     public float prevTimeInPortal;
+
     @Shadow
     public float timeInPortal;
+
     @Shadow
     protected Minecraft mc;
+
     @Shadow
     public MovementInput movementInput;
 
-    @Override
     @Shadow
-    public abstract void sendPlayerAbilities();
+    @Final
+    public NetHandlerPlayClient sendQueue;
+
+    @Shadow
+    private boolean serverSprintState;
+
+    @Shadow
+    private boolean serverSneakState;
+
+    @Shadow
+    private double lastReportedPosX;
+
+    @Shadow
+    private double lastReportedPosY;
+
+    @Shadow
+    private double lastReportedPosZ;
+
+    @Shadow
+    private float lastReportedYaw;
+
+    @Shadow
+    private float lastReportedPitch;
+
+    @Shadow
+    private int positionUpdateTicks;
+
+    @Shadow
+    private int offGroundTicks;
+
+    @Shadow
+    public abstract void setSprinting(boolean sprinting);
+
+    @Shadow
+    public abstract boolean isSneaking();
 
     @Shadow
     protected abstract boolean isCurrentViewEntity();
@@ -64,78 +93,63 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
     public abstract boolean isRidingHorse();
 
     @Shadow
-    private int horseJumpPowerCounter;
-    @Shadow
-    private float horseJumpPower;
-
-    @Shadow
     protected abstract void sendHorseJump();
 
     @Shadow
-    private boolean serverSprintState;
-    @Shadow
-    @Final
-    public NetHandlerPlayClient sendQueue;
+    public abstract void sendPlayerAbilities();
 
-    @Override
-    @Shadow
-    public abstract boolean isSneaking();
-
-    @Shadow
-    private boolean serverSneakState;
-    @Shadow
-    private double lastReportedPosX;
-    @Shadow
-    private double lastReportedPosY;
-    @Shadow
-    private double lastReportedPosZ;
-    @Shadow
-    private float lastReportedYaw;
-    @Shadow
-    private float lastReportedPitch;
-    @Shadow
-    private int positionUpdateTicks;
-    @Shadow
-    private int offGroundTicks;
-
-@Overwrite
-public void onUpdate() {
-    if (this.worldObj.isBlockLoaded(new BlockPos(this.posX, 0.0, this.posZ))) {
-        RotationUtils.prevRenderPitch = RotationUtils.renderPitch;
-        RotationUtils.prevRenderYaw = RotationUtils.renderYaw;
-
-        net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new PreUpdateEvent());
-
-        super.onUpdate();
-
-        // Update offGroundTicks
-        if (mc.thePlayer.onGround) {
-            this.setOffGroundTicks(0);
-        } else {
-            this.setOffGroundTicks(this.getOffGroundTicks() + 1);
-        }
-
-        if (this.isRiding()) {
-            this.sendQueue.addToSendQueue(new C03PacketPlayer.C05PacketPlayerLook(this.rotationYaw, this.rotationPitch, this.onGround));
-            this.sendQueue.addToSendQueue(new C0CPacketInput(this.moveStrafing, this.moveForward, this.movementInput.jump, this.movementInput.sneak));
-        } else {
-            this.onUpdateWalkingPlayer();
-        }
-
-        net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new PostUpdateEvent());
+    public MixinEntityPlayerSP(World worldIn, GameProfile playerProfile) {
+        super(worldIn, playerProfile);
     }
-}
-    
-// Getter for offGroundTicks
-public int getOffGroundTicks() {
-    return this.offGroundTicks;
-}
 
-// Setter for offGroundTicks
-public void setOffGroundTicks(int ticks) {
-    this.offGroundTicks = ticks;
-}
-    
+    // Getter for offGroundTicks
+    public int getOffGroundTicks() {
+        return this.offGroundTicks;
+    }
+
+    // Setter for offGroundTicks
+    public void setOffGroundTicks(int ticks) {
+        this.offGroundTicks = ticks;
+    }
+
+    @Overwrite
+    public void onUpdate() {
+        if (this.worldObj.isBlockLoaded(new BlockPos(this.posX, 0.0, this.posZ))) {
+            // Update rotation utilities
+            RotationUtils.prevRenderPitch = RotationUtils.renderPitch;
+            RotationUtils.prevRenderYaw = RotationUtils.renderYaw;
+
+            // Pre-update event
+            net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new PreUpdateEvent());
+
+            // Call the original update logic
+            super.onUpdate();
+
+            // Update offGroundTicks
+            if (mc.thePlayer.onGround) {
+                this.setOffGroundTicks(0);
+            } else {
+                this.setOffGroundTicks(this.getOffGroundTicks() + 1);
+            }
+
+            // Check if sprinting should be stopped
+            if (ModuleManager.tower != null && !ModuleManager.tower.canSprint()) {
+                this.setSprinting(false);
+            }
+
+            // Handle riding or walking
+            if (this.isRiding()) {
+                this.sendQueue.addToSendQueue(new C03PacketPlayer.C05PacketPlayerLook(this.rotationYaw, this.rotationPitch, this.onGround));
+                this.sendQueue.addToSendQueue(new C0CPacketInput(this.moveStrafing, this.moveForward, this.movementInput.jump, this.movementInput.sneak));
+            } else {
+                this.onUpdateWalkingPlayer();
+            }
+
+            // Post-update event
+            net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new PostUpdateEvent());
+        }
+    }
+
     @Overwrite
     public void onUpdateWalkingPlayer() {
         PreMotionEvent preMotionEvent = new PreMotionEvent(
@@ -174,49 +188,28 @@ public void setOffGroundTicks(int ticks) {
         }
 
         if (this.isCurrentViewEntity()) {
-            if (PreMotionEvent.setRenderYaw()) {
-                RotationUtils.setRenderYaw(preMotionEvent.getYaw());
-                preMotionEvent.setRenderYaw(false);
-            }
+            double deltaX = preMotionEvent.getPosX() - this.lastReportedPosX;
+            double deltaY = preMotionEvent.getPosY() - this.lastReportedPosY;
+            double deltaZ = preMotionEvent.getPosZ() - this.lastReportedPosZ;
+            boolean positionChanged = deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ > 9.0E-4 || this.positionUpdateTicks >= 20;
+            boolean rotationChanged = preMotionEvent.getYaw() != this.lastReportedYaw || preMotionEvent.getPitch() != this.lastReportedPitch;
 
-            RotationUtils.renderPitch = preMotionEvent.getPitch();
-            RotationUtils.renderYaw = preMotionEvent.getYaw();
-
-            double d0 = preMotionEvent.getPosX() - this.lastReportedPosX;
-            double d1 = preMotionEvent.getPosY() - this.lastReportedPosY;
-            double d2 = preMotionEvent.getPosZ() - this.lastReportedPosZ;
-            double d3 = preMotionEvent.getYaw() - this.lastReportedYaw;
-            double d4 = preMotionEvent.getPitch() - this.lastReportedPitch;
-            boolean flag2 = d0 * d0 + d1 * d1 + d2 * d2 > 9.0E-4 || this.positionUpdateTicks >= 20;
-            boolean flag3 = d3 != 0.0 || d4 != 0.0;
-            if (this.ridingEntity == null) {
-                if (flag2 && flag3) {
-                    this.sendQueue.addToSendQueue(new C03PacketPlayer.C06PacketPlayerPosLook(preMotionEvent.getPosX(), preMotionEvent.getPosY(), preMotionEvent.getPosZ(), preMotionEvent.getYaw(), preMotionEvent.getPitch(), preMotionEvent.isOnGround()));
-                } else if (flag2) {
-                    this.sendQueue.addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(preMotionEvent.getPosX(), preMotionEvent.getPosY(), preMotionEvent.getPosZ(), preMotionEvent.isOnGround()));
-                } else if (flag3) {
-                    this.sendQueue.addToSendQueue(new C03PacketPlayer.C05PacketPlayerLook(preMotionEvent.getYaw(), preMotionEvent.getPitch(), preMotionEvent.isOnGround()));
-                } else {
-                    this.sendQueue.addToSendQueue(new C03PacketPlayer(preMotionEvent.isOnGround()));
-                }
+            if (positionChanged && rotationChanged) {
+                this.sendQueue.addToSendQueue(new C03PacketPlayer.C06PacketPlayerPosLook(preMotionEvent.getPosX(), preMotionEvent.getPosY(), preMotionEvent.getPosZ(), preMotionEvent.getYaw(), preMotionEvent.getPitch(), preMotionEvent.isOnGround()));
+            } else if (positionChanged) {
+                this.sendQueue.addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(preMotionEvent.getPosX(), preMotionEvent.getPosY(), preMotionEvent.getPosZ(), preMotionEvent.isOnGround()));
+            } else if (rotationChanged) {
+                this.sendQueue.addToSendQueue(new C03PacketPlayer.C05PacketPlayerLook(preMotionEvent.getYaw(), preMotionEvent.getPitch(), preMotionEvent.isOnGround()));
             } else {
-                this.sendQueue.addToSendQueue(new C03PacketPlayer.C06PacketPlayerPosLook(this.motionX, -999.0D, this.motionZ, preMotionEvent.getYaw(), preMotionEvent.getPitch(), preMotionEvent.isOnGround()));
-                flag2 = false;
+                this.sendQueue.addToSendQueue(new C03PacketPlayer(preMotionEvent.isOnGround()));
             }
 
-            ++this.positionUpdateTicks;
-
-            if (flag2) {
-                this.lastReportedPosX = preMotionEvent.getPosX();
-                this.lastReportedPosY = preMotionEvent.getPosY();
-                this.lastReportedPosZ = preMotionEvent.getPosZ();
-                this.positionUpdateTicks = 0;
-            }
-
-            if (flag3) {
-                this.lastReportedYaw = preMotionEvent.getYaw();
-                this.lastReportedPitch = preMotionEvent.getPitch();
-            }
+            this.lastReportedPosX = preMotionEvent.getPosX();
+            this.lastReportedPosY = preMotionEvent.getPosY();
+            this.lastReportedPosZ = preMotionEvent.getPosZ();
+            this.lastReportedYaw = preMotionEvent.getYaw();
+            this.lastReportedPitch = preMotionEvent.getPitch();
+            this.positionUpdateTicks = 0;
         }
 
         net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new PostMotionEvent());
@@ -235,138 +228,6 @@ public void setOffGroundTicks(int ticks) {
             --this.sprintToggleTimer;
         }
 
-        this.prevTimeInPortal = this.timeInPortal;
-        if (this.inPortal) {
-            if (this.mc.currentScreen != null && !this.mc.currentScreen.doesGuiPauseGame()) {
-                this.mc.displayGuiScreen((GuiScreen) null);
-            }
-
-            if (this.timeInPortal == 0.0F) {
-                this.mc.getSoundHandler().playSound(PositionedSoundRecord.create(new ResourceLocation("portal.trigger"), this.rand.nextFloat() * 0.4F + 0.8F));
-            }
-
-            this.timeInPortal += 0.0125F;
-            if (this.timeInPortal >= 1.0F) {
-                this.timeInPortal = 1.0F;
-            }
-
-            this.inPortal = false;
-        } else if (this.isPotionActive(Potion.confusion) && this.getActivePotionEffect(Potion.confusion).getDuration() > 60) {
-            this.timeInPortal += 0.006666667F;
-            if (this.timeInPortal > 1.0F) {
-                this.timeInPortal = 1.0F;
-            }
-        } else {
-            if (this.timeInPortal > 0.0F) {
-                this.timeInPortal -= 0.05F;
-            }
-
-            if (this.timeInPortal < 0.0F) {
-                this.timeInPortal = 0.0F;
-            }
-        }
-
-        if (this.timeUntilPortal > 0) {
-            --this.timeUntilPortal;
-        }
-
-        boolean flag = this.movementInput.jump;
-        boolean flag1 = this.movementInput.sneak;
-        float f = 0.8F;
-        boolean flag2 = this.movementInput.moveForward >= f;
-        this.movementInput.updatePlayerMoveState();
-        boolean stopSprint = ModuleManager.noSlow == null || !ModuleManager.noSlow.isEnabled() || NoSlow.slowed.getInput() == 80;
-        if ((this.isUsingItem() || (ModuleManager.killAura != null && ModuleManager.killAura.isEnabled() && ModuleManager.killAura.block.get() && ModuleManager.killAura.rmbDown && ModuleManager.killAura.manualBlock.isToggled())) && !this.isRiding()) {
-            MovementInput var10000 = this.movementInput;
-            float slowed = NoSlow.getSlowed();
-            var10000.moveStrafe *= slowed;
-            var10000 = this.movementInput;
-            var10000.moveForward *= slowed;
-            if (stopSprint) {
-                this.sprintToggleTimer = 0;
-            }
-        }
-
-        this.pushOutOfBlocks(this.posX - (double) this.width * 0.35, this.getEntityBoundingBox().minY + 0.5, this.posZ + (double) this.width * 0.35);
-        this.pushOutOfBlocks(this.posX - (double) this.width * 0.35, this.getEntityBoundingBox().minY + 0.5, this.posZ - (double) this.width * 0.35);
-        this.pushOutOfBlocks(this.posX + (double) this.width * 0.35, this.getEntityBoundingBox().minY + 0.5, this.posZ - (double) this.width * 0.35);
-        this.pushOutOfBlocks(this.posX + (double) this.width * 0.35, this.getEntityBoundingBox().minY + 0.5, this.posZ + (double) this.width * 0.35);
-        boolean flag3 = (float) this.getFoodStats().getFoodLevel() > 6.0F || this.capabilities.allowFlying;
-        if (this.onGround && !flag1 && !flag2 && this.movementInput.moveForward >= f && !this.isSprinting() && flag3 && (!(this.isUsingItem() || (ModuleManager.killAura != null && ModuleManager.killAura.isEnabled() && ModuleManager.killAura.block.get() && ModuleManager.killAura.rmbDown && ModuleManager.killAura.manualBlock.isToggled())) || !stopSprint) && !this.isPotionActive(Potion.blindness)) {
-            if (this.sprintToggleTimer <= 0 && !this.mc.gameSettings.keyBindSprint.isKeyDown()) {
-                this.sprintToggleTimer = 7;
-            } else {
-                this.setSprinting(true);
-            }
-        }
-
-        if ((!this.isSprinting() && this.movementInput.moveForward >= f && flag3 && (!(this.isUsingItem() || (ModuleManager.killAura != null && ModuleManager.killAura.isEnabled() && ModuleManager.killAura.block.get() && ModuleManager.killAura.rmbDown && ModuleManager.killAura.manualBlock.isToggled())) || !stopSprint) && !this.isPotionActive(Potion.blindness) && this.mc.gameSettings.keyBindSprint.isKeyDown())) {
-            this.setSprinting(true);
-        }
-
-        if (this.isSprinting() && ((this.movementInput.moveForward < f || this.isCollidedHorizontally || !flag3) || (ModuleManager.scaffold != null && ModuleManager.scaffold.isEnabled() && !ModuleManager.scaffold.sprint() && !ModuleManager.tower.canSprint()) || (ModuleManager.wTap.isEnabled() && WTap.stopSprint))) {
-            this.setSprinting(false);
-            WTap.stopSprint = false;
-        }
-
-        if (this.capabilities.allowFlying) {
-            if (this.mc.playerController.isSpectatorMode()) {
-                if (!this.capabilities.isFlying) {
-                    this.capabilities.isFlying = true;
-                    this.sendPlayerAbilities();
-                }
-            } else if (!flag && this.movementInput.jump) {
-                if (this.flyToggleTimer == 0) {
-                    this.flyToggleTimer = 7;
-                } else {
-                    this.capabilities.isFlying = !this.capabilities.isFlying;
-                    this.sendPlayerAbilities();
-                    this.flyToggleTimer = 0;
-                }
-            }
-        }
-
-        if (this.capabilities.isFlying && this.isCurrentViewEntity()) {
-            if (this.movementInput.sneak) {
-                this.motionY -= (double) (this.capabilities.getFlySpeed() * 3.0F);
-            }
-
-            if (this.movementInput.jump) {
-                this.motionY += (double) (this.capabilities.getFlySpeed() * 3.0F);
-            }
-        }
-
-        if (this.isRidingHorse()) {
-            if (this.horseJumpPowerCounter < 0) {
-                ++this.horseJumpPowerCounter;
-                if (this.horseJumpPowerCounter == 0) {
-                    this.horseJumpPower = 0.0F;
-                }
-            }
-
-            if (flag && !this.movementInput.jump) {
-                this.horseJumpPowerCounter = -10;
-                this.sendHorseJump();
-            } else if (!flag && this.movementInput.jump) {
-                this.horseJumpPowerCounter = 0;
-                this.horseJumpPower = 0.0F;
-            } else if (flag) {
-                ++this.horseJumpPowerCounter;
-                if (this.horseJumpPowerCounter < 10) {
-                    this.horseJumpPower = (float) this.horseJumpPowerCounter * 0.1F;
-                } else {
-                    this.horseJumpPower = 0.8F + 2.0F / (float) (this.horseJumpPowerCounter - 9) * 0.1F;
-                }
-            }
-        } else {
-            this.horseJumpPower = 0.0F;
-        }
-
         super.onLivingUpdate();
-        if (this.onGround && this.capabilities.isFlying && !this.mc.playerController.isSpectatorMode()) {
-            this.capabilities.isFlying = false;
-            this.sendPlayerAbilities();
-        }
-
     }
 }
